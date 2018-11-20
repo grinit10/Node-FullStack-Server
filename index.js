9be9dffa12283b7c8f15cd1eebab72d283a0328e
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2018-11-20 22:13:42 +0530

    Triggering build

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res, next) => {
-    res.send({'hi':'Hello from Arnab'})
+    res.send({'hi':'Hello World!!'})
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
