9bc29c97b3b6616a1f14e19e013603b60f52b8b3
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2018-11-21 10:26:53 +0530

    Adding google oauth

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express');
-const app = express();
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
-app.get('/', (req, res, next) => {
-    res.send({'hi':'Hello World!!'})
-});
+const app = express();
+passport.use(new googleStrategy({
+    clientID : keys.googleClientID,
+    clientSecret : keys.googleClientSecret,
+    callbackURL : '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    console.log(accessToken);
+    console.log(refreshToken);
+    console.log(profile);
+    console.log(done);
+}));
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
