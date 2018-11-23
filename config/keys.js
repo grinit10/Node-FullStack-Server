359e046a3af6d53e3b6ec4f61442035cb97a20ce
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2018-11-23 16:16:06 +0530

    test

--- config/keys.js
+++ config/keys.js
@@ -2,6 +2,6 @@ if(process.NODE_ENV === 'production') {
     console.log('in prod');
     module.exports =require('./prod');
 } else {
-    // console.log(process.env);
+    console.log(process.NODE_ENV);
     module.exports =require('./dev');
 }
