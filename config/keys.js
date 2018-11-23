323c778e2f2ed98d8a74b090369a2d911f032715
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2018-11-23 16:03:36 +0530

    Adding debug

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,6 @@ if(process.env === 'production') {
     console.log('in prod');
     module.exports =require('./prod');
 } else {
+    console.log('in dev');
     module.exports =require('./dev');
 }
