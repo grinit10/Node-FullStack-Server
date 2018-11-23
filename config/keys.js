4a24cc88a24f3d2df134ea8c893ed37269af6558
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2018-11-23 16:11:39 +0530

    Furthur debugging

--- config/keys.js
+++ config/keys.js
@@ -2,6 +2,6 @@ if(process.env === 'production') {
     console.log('in prod');
     module.exports =require('./prod');
 } else {
-    console.log('in dev');
+    console.log(process.env);
     module.exports =require('./dev');
 }
