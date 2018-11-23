2784c74a2323aac37cdfb5d2b5c3959462ececc3
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2018-11-23 16:01:16 +0530

    Adding dev.js

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+    googleClientID : '904809597141-7fmtqb15gtg5htp4aln3rkqbbtblik46.apps.googleusercontent.com',
+    googleClientSecret: 'xaqUW1pIKg2iGWNyhKnFWa5X',
+    mongoHostUri: 'mongodb+srv://nodeapicluster-zaytg.mongodb.net/test?retryWrites=true',
+    mongoUser: 'arnab',
+    mongoPassword: 'Nov@2018',
+    dbName: 'NodeFullStackDb-dev',
+    cookieKey: 'kjasfsdkjbfkshfksckvbskjzbvkjsbkfdsckjslbas'
+}
