319e3a7f47f6ca42102bf386337f882150430311
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2018-11-24 11:12:54 +0530

    Adding client with redux setup

--- services/passport.js
+++ services/passport.js
@@ -11,41 +11,27 @@ passport.serializeUser((user, done) => {
 
 passport.deserializeUser((id, done) => {
     Users
-    .findById(id)
-    .then(user => done(null, user))
-    .catch(err => console.log(err))
+        .findById(id)
+        .then(user => done(null, user))
+        .catch(err => console.log(err))
 })
 
 passport.use(new googleStrategy({
-    clientID : keys.googleClientID,
-    clientSecret : keys.googleClientSecret,
-    callbackURL : '/auth/google/callback',
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback',
     proxy: true
-}, (accessToken, refreshToken, profile, done) => {
+}, async (accessToken, refreshToken, profile, done) => {
     // console.log(accessToken);
     // console.log(refreshToken);
     // console.log(profile);
     // console.log(done);
-    Users
-    .findOne({googleId: profile.id})
-    .then(user => {
-        if(user){
-            console.log('A record for the same user exists with google id :' + user.googleId);
-            done(null, user);
-        }
-        else{
-            new Users({googleId: profile.id})
-            .save()
-            .then(user => {
-                console.log('User saved to collection');
-                done(null, user);
-            })
-            .catch(err => {
-                console.log(err);
-                done(err);
-            })
-        }
-    })
-    .catch(err => console.log(err));
-    
+    const user = await Users.findOne({ googleId: profile.id });
+    if (user) {
+        console.log('A record for the same user exists with google id :' + user.googleId);
+        return done(null, user);
+    }
+    user = await new Users({ googleId: profile.id }).save();
+    done(null, user);
+
 }));
