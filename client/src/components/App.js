d95f024e73ddc6e0bb7552d7499d2f0605620443
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2018-11-24 12:15:20 +0530

    Adding navigation

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
+
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 const app = () => {
-    return(
-        <div>
-            Hi there!!
+    return (
+        <div className='container'>
+            <BrowserRouter>
+                <div>
+                    <Header></Header>
+                    <Route path='/surveys' component={Dashboard} exact></Route>
+                    <Route path='/surveys/New' component={SurveyNew}></Route>
+                    <Route path='/' component={Landing} exact></Route>
+                </div>
+            </BrowserRouter>
         </div>
     )
 }
